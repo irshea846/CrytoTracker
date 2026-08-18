1bc434dedb4ba86c606924df23f9a94fd4a56ba4
Richard Shea <irshea846@gmail.com>
2026-08-18 09:17:00 -0700

    Day13: Multiplatform ViewModelling The Goal: Inject the official Jetpack Lifecycle ViewModel Multiplatform library dependency into your version catalog. Create your shared CryptoListViewModel.kt class inside your domain-presentation architecture tree.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -71,6 +71,7 @@ kotlin {
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
         }
